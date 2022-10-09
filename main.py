f816e1e1d64612dbf61b2600ee23df4e5571ec90
Mike Bridge <mike@bridgecanada.com>
2022-10-09 11:52:36 -0600

    auto-name output

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
 #!/usr/bin/env python3
 
 import io
-#import os
 import argparse
-#import pathlib
+import pathlib
+import sys
+
 import webvtt
 from webvtt import WebVTT
 
 
-#from datetime import datetime
-#from functools import partial
-#from typing import Callable, List, Optional, TypedDict, Union
-#from webvtt import WebVTT, Caption
-
-#import orjson.orjson
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# SCOPES = 'https://www.googleapis.com/auth/drive.readonly.metadata'
-#
-#
-# def verify_env_or_exit() -> None:
-#     if not FOLDER:
-#         exit('Please set GOOGLE_DRIVE_FOLDER in your .env file')
-#     if not FOLDER_ID:
-#         exit('Please set GOOGLE_DRIVE_FOLDER_ID in your .env file')
-#     if not RAW_FILES_FOLDER:
-#         exit('Please set RAW_FILES_FOLDER in your .env file')
-#     if not RAW_PATH.exists():
-#         exit(f"could not find RAW_FILES_FOLDER: {RAW_FILES_FOLDER}")
+def create_file_name(orig_file_name: str, ext: str = 'all') -> str:
+    p = pathlib.Path(orig_file_name)
+    stem = p.stem
+    orig_file_extension = p.suffix
+    new_file_name = pathlib.Path(stem).stem
+    return f'{new_file_name}.{ext}{orig_file_extension}'
 
 
 def main(files):
     vtt = webvtt.WebVTT();
     caption_sets: list[WebVTT] = []
+    if len(files) == 0:
+        sys.stderr.write('there must be at least one file')
+        exit(1)
     for file in files:
         print(f'reading {file}')
         caption_sets.append(webvtt.read(file))
@@ -42,14 +30,8 @@ def main(files):
     print(f'there are {len(caption_sets)} sets')
     combined_captions = zip(*[c.captions for c in caption_sets])
     # combined_captions = zip(caption_sets[0].captions, caption_sets[1].captions)
-    print(combined_captions)
+    # print(combined_captions)
     for caption_set in combined_captions:
-        print('found set')
-        print(caption_set)
-        # for caption_pair in caption_set:
-        #      print(caption_pair)
-        # print(caption_set[0].start)
-        # print(caption_set[0].end)
         row = 0
         new_captions = []
         for caption in caption_set:
@@ -60,44 +42,15 @@ def main(files):
                 new_captions.append(caption.text)
         caption = webvtt.Caption(caption_set[0].start, caption_set[0].end, new_captions)
         vtt.captions.append(caption)
-    # open('new_captions.vtt', 'w', encoding='utf8') as fd:
-    with io.open('new_captions.vtt', 'w', encoding='utf8') as fd:
+    new_file_name = create_file_name(files[0]) or 'new_captions.vtt'
+    print(f'writing to {new_file_name}')
+    with io.open(new_file_name, 'w', encoding='utf8') as fd:
         vtt.write(fd)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download Files from Google Drive')
-    # parser.add_argument('integers', metavar='N', type=int, nargs='+',
-    #                 help='an integer for the accumulator')
-    # parser.add_argument('-m', '--max-files',
-    #                     type=int,
-    #                     help='max number of files to download',
-    #                     required=False)
-#     parser.add_argument('-d', '--days',
-#                         type=int,
-#                         default=GOOGLE_DRIVE_DAYS_AGO,
-#                         help='download files uploaded in last N days',
-#                         required=False)
-
-    parser.add_argument('-f','--file', nargs='+', help='vtt file', required=True)
+    parser.add_argument('-f', '--file', nargs='+', help='vtt file', required=True)
 
     args = parser.parse_args()
-    # print type of args.max_files
-    # print(f'max-files is        {args.max_files}')
-    #print(f'days-ago is        {args.file}')
-    # files_to_download = prune_tree(all_files, partial(is_file_newer_than, since_time))
-    # print(f'== TOTAL: {count_files(files_to_download)}')
-    # all_files_downloaded = []
-    #     for file_or_dir in files_to_download:
-    #         # print(f'== DOWNLOADING DIR {file_or_dir["name"]}')
-    #         # download_tree(file, DOWNLOAD_DIR)
-    #         downloaded_files = download_tree(file_or_dir, RAW_PATH, partial(download_file, google_drive))
-    #         for file in downloaded_files:
-    #             all_files_downloaded.append(file)
-    #             print(f'== DOWNLOADED {file["full_path"]}')
-    #     print(f"  FILES DOWNLOADED:{len(all_files_downloaded)}")
     main(args.file)
-
-#parser.print_help()
-
-#print(args.accumulate(args.integers))
